1fe0a0c51f29ec1cb31671c0c72e0df8e4bf5ae6
NmnGit19 <namangaur49@gmail.com>
2021-12-26 21:10:29 +0530

    change extra space in line 153

---
+++ Signup form/script.js
@@ -0,0 +1,6 @@
+function Signupcolor() {
+    document.getElementById("but1").style.backgroundColor = "green";
+}
+function logincolor() {
+    document.getElementById("but2").style.backgroundColor = "grey";
+}
